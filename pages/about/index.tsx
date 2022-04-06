7fb95340ca2c8831c1883d746a462cbae52a1d59
takayanagi <ashday7188@gmail.com>
2022-04-07 01:35:18 +0900

    [Add] product ページ実装

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,23 +1,48 @@
 import Layout from '../../components/layout/layout';
 import styles from './about.module.scss';
 
+const haveBeenEngineer = (() => {
+  const current = new Date();
+  const currentYear = current.getFullYear();
+  const currentMonth = (current.getMonth() + 1);
+  const start = new Date(2020, 9);
+  const startYear = start.getFullYear();
+  const startMonth = (start.getMonth());
+  console.log('current', currentMonth);
+  console.log('startMonth', startMonth);
+
+  return `${currentYear - startYear}年${Math.abs((currentMonth < startMonth ? 12 + currentMonth : currentMonth) - startMonth)}ヶ月`;
+})();
+
 export default function About() {
   return (
     <Layout>
       <div className={styles['about']}>
         <h2>自己紹介</h2>
-        <p>フロントエンドエンジニア</p>
-        <p>Node.js でバックエンドをやったりもします</p>
+        <p>1991年生まれ</p>
+        <p>愛媛大学理学部化学科中退</p>
+        <p>フロントエンドエンジニア ({haveBeenEngineer})</p>
+        <p>
+          仕事では electron x react でデスクトップアプリつくってるのがいちばん長い気がします。<br />
+          他にも react で web をやったり、create.js で ゲームつくったり。<br />
+          Node.js (express)でバックエンドをやったりしたこともあります。
+        </p>
+        <p>TDD を勉強中</p>
+        <p>個人開発で自動でお金を稼ぐアプリをつくって働かずに生きていくためにプログラマになりましたが、現実は残業しまくりのサラリーマンプログラマです😇</p>
+        <p>小説とプログラミングが好きです☕</p>
         <h2>よく使う</h2>
         <ul>
           <li>typescript</li>
-          <li>react</li>
+          <li>react / next.js</li>
           <li>electron</li>
+          <li>Node.js</li>
           <li>express</li>
         </ul>
-        <h2>仕事</h2>
-        <p>普段は React で web やったり、Electron でアプリやったりしてます。</p>
-        <p>個人開発で自動でお金を稼ぐアプリをつくって働かずに生きていくためにプログラマになりましたが、現実は残業しまくりのサラリーマンプログラマです😇</p>
+        <h2>メディア</h2>
+        <ul>
+          <li><a href="https://mobile.twitter.com/gia_sorairo">twitter</a></li>
+          <li><a href="https://zenn.dev/gia">zenn</a></li>
+        </ul>
       </div>
     </Layout>
   )
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,11 +1,30 @@
 import Layout from '../../components/layout/layout';
+import styles from './product.module.scss';
 
-export default function About() {
+export default function Product() {
   return (
     <Layout>
-      <div>
+      <div className={styles['product']}>
+        <div className={styles['product_title']}>つくった</div>
         <h2>アンダーラインを教えて</h2>
+        <div className={styles['img_container']}>
+          <img src="./images/product/your-underline.svg" alt="your-underline" />
+        </div>
+        <h3>link</h3>
+        <p><a href="https://yourunderline.net">https://yourunderline.net</a></p>
+        <h3>概要</h3>
+        <p>好きな小説や漫画の一文を twitter で共有するサービスです。</p>
         <h2>youtube thumbnails getter</h2>
+        <div className={styles['img_container']}>
+          <img src="./images/product/youtube-thmubnails-getter.svg" alt="" />
+        </div>
+        <h3>link</h3>
+        <p><a href="https://chrome.google.com/webstore/detail/youtube-thumbnails-getter/ghhbhfmihfneeonmbchmkfpdcnmolfgo">youtube thumbnails getter</a></p>
+        <h3>概要</h3>
+        <p>
+          Youtube で動画を観ながらその動画のサムネイルを確認できる拡張機能です。<br />
+          Youtube で動画を観てるときに、「この動画のサムネイルってどうやって確認するんだ？」ってなってつくりました。
+        </p>
       </div>
     </Layout>
   )
