af988cac50e27fc989b1d218b5e0b6a3acbc575b
yanagi <takayanagi.t@marietta.co.jp>
2022-10-16 18:44:34 +0900

    [Add] flex done

--- src/components/page/projects/css/slug/flex/flex.tsx
+++ src/components/page/projects/css/slug/flex/flex.tsx
@@ -40,7 +40,8 @@ export const Flex = () => {
         そういう基本を理解して、しっかり flex をつかえるようになりたい。
         あと flex 関連で知らないプロパティがたくさんあると思うので目を通しておきたい。
       </p>
-      <h2>とりあえず mdn で flex のページを読む</h2>
+
+      <h3>flex</h3>
       <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex">mdn - flex</a></p>
       <p>
         mdn によると「flex は CSS の一括プロパティで、フレックスアイテムをフレックスコンテナの領域に収めるために、どのように伸張・収縮させるかを指定します」
@@ -50,7 +51,9 @@ export const Flex = () => {
       <h3>flex-grow</h3>
       <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex">mdn - flex-grow</a></p>
       <q>
-        flex-grow は CSS のプロパティで、フレックスアイテムの主軸方向の寸法のフレックス<code>伸長係数</code>を設定します。
+        <p>
+          flex-grow は CSS のプロパティで、フレックスアイテムの主軸方向の寸法のフレックス<code>伸長係数</code>を設定します。
+        </p>
         <div className={styles['quotation_from']}>
           <a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex">mdn - flex-grow</a>
         </div>
@@ -59,7 +62,9 @@ export const Flex = () => {
         <code>フレックス身長係数</code>というのは、フレックスコンテナ内の残りに空間のうち、どれだけがそのアイテムに割り当てられるかを表した値らしい。
       </p>
       <q>
-        <code>主軸方向の寸法</code>は、アイテムの幅または高さのどちらかで、<code>flex-direction</code>の値に依存して決まります。
+        <p>
+          <code>主軸方向の寸法</code>は、アイテムの幅または高さのどちらかで、<code>flex-direction</code>の値に依存して決まります。
+        </p>
         <div className={styles['quotation_from']}>
           <a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex">mdn - flex-grow</a>
         </div>
@@ -85,14 +90,16 @@ export const Flex = () => {
       <h3>flex-shrink</h3>
       <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-shrink">mdn - flex-shrink</a></p>
       <q>
-        <code>flex-shrink</code> は CSS のプロパティで、フレックスアイテムの <code>縮小係数</code>を設定します。
-        すべてのフレックスアイテムの寸法がフレックスコンテナよりも大きい場合、アイテムは<code>flex-shrink</code>の数値にしたがって縮小して収まります。
-        <br />
-        使用時は<code>flex-shrink</code>は<code>flex-grow</code>や<code>flex-basis</code>などの他のフレックスプロパティとともに使用され、
-        ふつうは<code>flex</code>の一括指定を使用して定義されます。
-        <div className={styles['quotation_from']}>
+        <p>
+          <code>flex-shrink</code> は CSS のプロパティで、フレックスアイテムの <code>縮小係数</code>を設定します。
+          すべてのフレックスアイテムの寸法がフレックスコンテナよりも大きい場合、アイテムは<code>flex-shrink</code>の数値にしたがって縮小して収まります。
+          <br />
+          使用時は<code>flex-shrink</code>は<code>flex-grow</code>や<code>flex-basis</code>などの他のフレックスプロパティとともに使用され、
+          ふつうは<code>flex</code>の一括指定を使用して定義されます。
+        </p>
+        <p className={styles['quotation_from']}>
           <a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-shrink">mdn - flex-shrink</a>
-        </div>
+        </p>
       </q>
       <p>
         <code>flex-grow</code> が<code>伸長係数</code>を設定するのに対して、<code>flex-shrink</code> は<code>縮小係数</code>を設定する。なるほど。
@@ -114,11 +121,135 @@ export const Flex = () => {
       </p>
       <p>
         おー。<code>flex-shrink: 2</code>にしている item が、<code>flex-shrink: 1</code>にしている item より縮小している。
-        ちなみに<code>flex-shrink: 1</code>を記述を消してもレイアウトは変わらなかったので<code>flex-shrink</code> の default value が 1 だというのも体感した。
+        ちなみに<code>flex-shrink: 1</code>の記述を消してもレイアウトは変わらなかったので<code>flex-shrink</code> の default value が 1 だというのも体感した。
       </p>
 
       <h3>flex-basis</h3>
       <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-basis">mdn - flex-basis</a></p>
+      <q>
+        <p>
+          <code>flex-basis</code>は CSS のプロパティで、フレックスアイテムの主要部分の初期の寸法を設定します。
+          <code>box-sizing</code>で設定していない限り、このプロパティはコンテンツボックスの寸法を定義します。
+        </p>
+        <p className={styles['quotation_from']}>
+          <a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-basis">mdn - flex-basis</a>
+        </p>
+      </q>
+      <p>
+        フレックスアイテムの寸法を設定するってのはわかったけど、「box-sizingで設定しない限り、このプロパティはコンテンツボックスの寸法を定義します」がよくわかなんないな。
+        コンテンツボックスっていう単語を知らないからか。
+      </p>
+      <p>これ読めばわかるっぽいけど、ちょっといまはいいや。</p>
+      <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/box-sizing#:~:text=content%2Dbox%20%E3%81%AF%20CSS%20%E3%81%AE,%E3%82%88%E3%82%8A%E3%82%82%E5%BA%83%E3%81%8F%E3%81%AA%E3%82%8A%E3%81%BE%E3%81%99%E3%80%82">mdn - box-sizing</a></p>
+      <p>とりあえず flex-basis で flex item のサイズを指定できるらしい。で、mdn によると width よりも flex-basis のほうが優先られるらしい。</p>
+
+      <div className={styles['sandbox-3']}>
+        <div className={styles['flex_container']}>
+          <div className={styles['flex_item-1']}>flex-basis: 50px</div>
+          <div className={styles['flex_item-2']}>flex-basis: 100px</div>
+          <div className={styles['flex_item-3']}>flex-basis: 200px</div>
+        </div>
+      </div>
+
+      <p>まあなんとなくわかった。</p>
+      <p>あと flex-wrap ってのも大事そうなので見ておきたい。</p>
+
+      <h3>flex-wrap</h3>
+      <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-wrap">mdn - flex-wrap</a></p>
+      <q>
+        <p>
+          flex-wrap は css のプロパティで、フレックスアイテムを単一行に押し込むか、あるいは複数行に折り返してもよいかを指定します。
+          折返しを許可する場合は、行を積み重ねる方向の制御も可能です。
+        </p>
+        <p className={styles['quotation_from']}><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-wrap">mdn - flex-wrap</a></p>
+      </q>
+      <p><code>flex-wrap: wrap</code>で、折り返して、<code>flex-wrap: nowrap</code>で折り返さないってくらいか。これは知ってたな。</p>
+
+      <p>
+        w3 school の flexbox のページをみてて思ったけど、flexbox 関連のプロパティって、
+        flex container の設定をするプロパティと flex item の設定をするプロパティにわけられるのか。
+      </p>
+      <p><a href="https://www.w3schools.com/css/css3_flexbox.asp">w3school - flexbox</a></p>
+
+      <h3>flex container の設定をするプロパティ</h3>
+      <ul>
+        <li>flex-direction</li>
+        <li>flex-wrap</li>
+        <li>flex-flow</li>
+        <li>justify-content</li>
+        <li>align-items</li>
+        <li>align-content</li>
+      </ul>
+
+      <h3>flex item の設定をするプロパティ</h3>
+      <ul>
+        <li>order</li>
+        <li>flex-grow</li>
+        <li>flex-shrink</li>
+        <li>flex-basis</li>
+        <li>flex</li>
+        <li>align-self</li>
+      </ul>
+
+      <p>こうやって実際に分けて並べてみるとちょっと整理できた気持ちになるな。</p>
+
+      <p>flex-flow ってなんだろ。使ったことないな。</p>
+      <h3>flex-flow</h3>
+      <p><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-flow">mdn - flex-flow</a></p>
+      <p>あー。<code>flex-direction</code>と<code>flex-wrap</code>を一括で設定できるのか。</p>
+      <p>あと align-self についてもちょっと調べておきたいんだよな。なんか使ってはみるけど、いつもうまく動いてくれないから、正しい使い方を知っておきたい。</p>
+
+      <q>
+        <p>
+          align-self は CSS のプロパティで、グリッドやフレックスのアイテムの align-items の値を上書きします。
+          グリッドでは、アイテムはグリッド領域内で配置されます。フレックスボックスでは、アイテムは交差軸上で配置されます。
+        </p>
+        <p className={styles['quotation_from']}><a href="https://developer.mozilla.org/ja/docs/Web/CSS/flex-flow">mdn - flex-flow</a></p>
+      </q>
+
+      <p>交差軸って言葉がわからん。調べる。</p>
+      <p><a href="https://developer.mozilla.org/ja/docs/Glossary/Cross_Axis">mdn - cross axis</a></p>
+      <q>
+        <p>
+          フレックスボックスにおける交差軸(cross axis / クロス軸)は、主軸(main axis / メイン軸)で、
+          <code>flex-direction</code> が row または　<code>row-reverse</code> であるとき(つまり主軸が行方向であるとき)、列方向の軸のことです。
+        </p>
+        <p>
+          主軸が column または column-reverse の場合は、交差軸は行方向となります。
+        </p>
+        <p className={styles['quotation_from']}><a href="https://developer.mozilla.org/ja/docs/Glossary/Cross_Axis">mdn - cross axis</a></p>
+      </q>
+      <p>主軸に対する交差軸ってことか。</p>
+
+      <p className={styles['text_center']}>flex-direction: row (主軸: 横, 交差軸: 縦)</p>
+      <div className={styles['sandbox-4']}>
+        <div className={styles['flex_container_row']}>
+          <div className={styles['flex_item-1']}>center</div>
+          <div className={styles['flex_item-2']}>start</div>
+          <div className={styles['flex_item-3']}>end</div>
+        </div>
+      </div>
+
+      <p className={styles['text_center']}>flex-direction: column (主軸: 縦, 交差軸: 横)</p>
+      <div className={styles['sandbox-5']}>
+        <div className={styles['flex_container_column']}>
+          <div className={styles['flex_item-1']}>center</div>
+          <div className={styles['flex_item-2']}>start</div>
+          <div className={styles['flex_item-3']}>end</div>
+        </div>
+      </div>
+
+      <p>いったん <code>center</code>, <code>start</code>, <code>end</code>くらい使えれば大丈夫かな。</p>
+
+      <p>とりあえず flexbox はこれくらいでいいや。</p>
+      <p>学習過程をだらだらと書きながら進めていくスタイルでやってみたけど、なんか無駄が多いなあった気もした。</p>
+      <p>でもこうやって書きながら、こまめに deploy して公開してたからこそ、ひととおり最後までできたのでよかった、のかな？</p>
+      <p>
+        あとレイアウト系は <code>gird</code>学習しないとな。
+        それにしても、やっぱりなにかを作りながらの勉強と比べて、純粋に勉強だけするのは大変つまらないなというのは感じたな。
+        やっぱり何か作りながらがいいのかな。学習方法もいろいろ考えないとな。
+      </p>
+      <p>おしまい！</p>
     </div>
   );
 };
