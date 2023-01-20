58dae847de777b8a0faae7389d2ae8f6292ff78d
yanagi <ashday7188@gmail.com>
2023-01-21 05:58:34 +0900

    [Modify] layout

--- src/components/functional/adsense/adsense.tsx
+++ src/components/functional/adsense/adsense.tsx
@@ -1,7 +1,12 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export const Adsense = () => {
+type Props = {
+  innerWidth?: number;
+};
+
+export const Adsense = (props: Props) => {
+  const { innerWidth } = props;
   const { asPath } = useRouter();
 
   useEffect(() => {
@@ -10,18 +15,23 @@ export const Adsense = () => {
     } catch (error) {
       console.log(error);
     }
-  }, [asPath]);
+  }, [asPath, innerWidth]);
 
   return (
     <div key={asPath}>
       {/* <!-- flow-flow-flow_responsive --> */}
-      <ins className="adsbygoogle"
-        style={{ display: "block", textAlign: "center" }}
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: "block",
+          textAlign: "center",
+          height: "150px",
+        }}
         data-ad-client="ca-pub-7112973654947785"
         data-ad-slot="3648592048"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
+        // data-ad-format="auto"
+        // data-full-width-responsive="true"
       />
     </div>
-  )
+  );
 };
